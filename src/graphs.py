4a834196b8f2fdc3996da8329a3345a84c97b523
kof-coder <koforiac@students.kennesaw.edu>
2025-10-20 16:23:23 -0400

    Update graphs.py

--- src/graphs.py
+++ src/graphs.py
@@ -1,1 +1,18 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self, n=0):
+        self.n = n
+        self.adj = [[] for _ in range(n)]
+
+    def add_edge(self, u, v):
+        if u == v: return
+        self.adj[u].append(v)
+        self.adj[v].append(u)
+
+    @classmethod
+    def from_edge_list(cls, n, edges):
+        g = cls(n)
+        for u, v in edges: g.add_edge(u, v)
+        return g
 
