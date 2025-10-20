a322fadff911b7ce79648c8769209f282223ea0e
kof-coder <koforiac@students.kennesaw.edu>
2025-10-20 16:31:35 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+from utils.io import load_edgelist
+from graphs import Graph
+from ht_planarity.hopcroft_tarjan import HopcroftTarjanPlanarity
+from brute_force.naive_planarity import naive_is_planar
+
+def run(path):
+    n, edges = load_edgelist(path)
+    G = Graph.from_edge_list(n, edges)
+    ht = HopcroftTarjanPlanarity(G)
+    is_planar_ht = ht.is_planar()
+    is_planar_naive = naive_is_planar(n, edges)  # baseline
+    print(f"HT: {is_planar_ht} | Baseline: {is_planar_naive}")
+
+if __name__ == "__main__":
+    run("data/samples/k5.edgelist")
 
