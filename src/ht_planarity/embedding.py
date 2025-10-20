57425b69d592b80f90d17869d24a703408aeafcf
kof-coder <koforiac@students.kennesaw.edu>
2025-10-20 16:27:15 -0400

    Update embedding.py

--- src/ht_planarity/embedding.py
+++ src/ht_planarity/embedding.py
@@ -1,1 +1,9 @@
+class HalfEdge:
+    __slots__ = ("u","v","mate","next","prev")
+    def __init__(self,u,v): self.u,self.v,self.mate,self.next,self.prev = u,v,None,None,None
+
+class Embedding:
+    # TODO: maintain circular order of incident edges per vertex
+    # TODO: merge constraints during DFS backtracking
+    pass
 
