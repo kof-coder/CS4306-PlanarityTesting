911db92f5bf14cfe04cdcc2198192eb1c0b55e9d
kof-coder <koforiac@students.kennesaw.edu>
2025-10-28 15:34:30 -0400

    Update graphs.py

--- src/graphs.py
+++ src/graphs.py
@@ -1,18 +1,35 @@
+# src/graph.py
+
 from collections import defaultdict
 
 class Graph:
     def __init__(self, n=0):
+        """
+        n: number of vertices, assumed labeled 0..n-1
+        adj: adjacency list, adj[u] is a list of neighbors of u
+        """
         self.n = n
         self.adj = [[] for _ in range(n)]
 
     def add_edge(self, u, v):
-        if u == v: return
+        """
+        Add an undirected edge between u and v.
+        Ignores self-loops of the form (u,u).
+        """
+        if u == v:
+            return
         self.adj[u].append(v)
         self.adj[v].append(u)
 
     @classmethod
     def from_edge_list(cls, n, edges):
+        """
+        Build a Graph from:
+        n     = number of vertices
+        edges = list of (u,v) tuples
+        """
         g = cls(n)
-        for u, v in edges: g.add_edge(u, v)
+        for (u, v) in edges:
+            g.add_edge(u, v)
         return g
 
